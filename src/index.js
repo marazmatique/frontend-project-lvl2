cd1daa277cb3f1abd0ad347dc4d94c5102f24981
marazmatique <fxpnkt@gmail.com>
2020-01-31 22:29:58 +0300

    6th step completed

--- src/index.js
+++ src/index.js
@@ -1,20 +1,20 @@
 import commander from 'commander';
-import fs from 'fs';
 import getDiff from './getDiff';
 
-const gendiff = new commander.Command();
-gendiff
-  .version('0.0.1')
-  .description('fdfdf')
-  .option('-f, --format [style]', 'output format')
-  .arguments('<before>')
-  .arguments('<after>')
-  .action((before, after) => {
-    const beforePath = fs.readFileSync(before, 'utf8');
-    const afterPath = fs.readFileSync(after, 'utf8');
-    const diff = getDiff(beforePath, afterPath);
-    console.log(diff);
-  })
-  .parse(process.argv);
+export default () => {
+  const gendiff = new commander.Command();
 
-export default gendiff;
+  gendiff
+    .version('0.0.1')
+    .description('fdfdf')
+    .option('-f, --format [style]', 'output format')
+    .arguments('<before>')
+    .arguments('<after>')
+    .action((before, after) => {
+      const diff = getDiff(before, after);
+      console.log(diff);
+    })
+    .parse(process.argv);
+
+  return gendiff;
+};
