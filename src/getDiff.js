2ae8159851ea676e96446729f42f9511d424160d
marazmatique <fxpnkt@gmail.com>
2020-02-23 19:01:50 +0300

    added a new option for data output refactor function buildAst

--- src/getDiff.js
+++ src/getDiff.js
@@ -2,6 +2,7 @@ import _ from 'lodash';
 import { getData } from './parsers';
 import totalDiff from './formatters/total';
 import plainDiff from './formatters/plain';
+import jsonDiff from './formatters/json';
 
 export const isObject = (ele) => typeof ele === 'object' && ele !== null;
 
@@ -12,33 +13,33 @@ const buildAst = (obj1, obj2) => {
     const after = obj2[key];
 
     if (isObject(before) && isObject(after)) {
-      acc.push([`    ${key}`, buildAst(before, after)]);
+      acc.push([key, ' ', buildAst(before, after)]);
       return acc;
     }
     if (before === after) {
-      acc.push([`    ${key}`, before]);
+      acc.push([key, ' ', before]);
       return acc;
     }
-    if (_.has(obj1, key) && before !== after) {
-      acc.push([`  - ${key}`, before]);
+    if (_.has(obj1, key)) {
+      acc.push([key, '-', before]);
     }
-    if (_.has(obj2, key) && before !== after) {
-      acc.push([`  + ${key}`, after]);
+    if (_.has(obj2, key)) {
+      acc.push([key, '+', after]);
     }
 
     return acc;
   }, []);
 };
 
+const outputFormat = {
+  total: (ast) => totalDiff(ast),
+  plain: (ast) => plainDiff(ast),
+  json: (ast) => jsonDiff(ast),
+};
+
 export default (pathToConfigFile1, pathToConfigFile2, format) => {
   const obj1 = getData(pathToConfigFile1);
   const obj2 = getData(pathToConfigFile2);
   const ast = buildAst(obj1, obj2);
-  if (format === 'total') {
-    return totalDiff(ast);
-  }
-  if (format === 'plain') {
-    return plainDiff(ast);
-  }
-  return null;
+  return outputFormat[format](ast);
 };
