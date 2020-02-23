9e92b9eaefbaaebdaa46691d2d5a6c7061f4a0af
marazmatique <fxpnkt@gmail.com>
2020-02-23 18:55:41 +0300

    clean & rename test files

--- __tests__/getDiff.test.js
+++ __tests__/getDiff.test.js
@@ -14,7 +14,7 @@ const getFixturesPath = (name, ext) => path.format({
 });
 
 const makeConfig = {
-  '.json': (data) => JSON.stringify(data),
+  '.json': (data) => JSON.stringify(data, null, 2),
   '.yml': (data) => yaml.dump(data),
   '.ini': (data) => ini.encode(data),
 };
@@ -37,25 +37,37 @@ afterAll(() => {
 });
 
 test.each(['.json', '.yml', '.ini'])('plain%s', (extension) => {
-  const pathBefore = saveConfigFile('/Users/imac/Hexlet/frontend-project-lvl2/__fixtures__/before_havy.json',
+  const pathBefore = saveConfigFile('/Users/imac/Hexlet/frontend-project-lvl2/__fixtures__/before.json',
     extension);
-  const pathAfter = saveConfigFile('/Users/imac/Hexlet/frontend-project-lvl2/__fixtures__/after_havy.json',
+  const pathAfter = saveConfigFile('/Users/imac/Hexlet/frontend-project-lvl2/__fixtures__/after.json',
     extension);
 
   const answer = fs
-    .readFileSync('/Users/imac/Hexlet/frontend-project-lvl2/__fixtures__/answer_havy_plain', 'utf8');
+    .readFileSync('/Users/imac/Hexlet/frontend-project-lvl2/__fixtures__/answer_plain', 'utf8');
 
   expect(getDiff(pathBefore, pathAfter, 'plain')).toEqual(answer);
 });
 
 test.each(['.json', '.yml', '.ini'])('total%s', (extension) => {
-  const pathBefore = saveConfigFile('/Users/imac/Hexlet/frontend-project-lvl2/__fixtures__/before_havy.json',
+  const pathBefore = saveConfigFile('/Users/imac/Hexlet/frontend-project-lvl2/__fixtures__/before.json',
     extension);
-  const pathAfter = saveConfigFile('/Users/imac/Hexlet/frontend-project-lvl2/__fixtures__/after_havy.json',
+  const pathAfter = saveConfigFile('/Users/imac/Hexlet/frontend-project-lvl2/__fixtures__/after.json',
     extension);
 
   const answer = fs
-    .readFileSync('/Users/imac/Hexlet/frontend-project-lvl2/__fixtures__/answer_complicated', 'utf8');
+    .readFileSync('/Users/imac/Hexlet/frontend-project-lvl2/__fixtures__/answer_total', 'utf8');
 
   expect(getDiff(pathBefore, pathAfter, 'total')).toEqual(answer);
+});
+
+test.each(['.json', '.yml', '.ini'])('json%s', (extension) => {
+  const pathBefore = saveConfigFile('/Users/imac/Hexlet/frontend-project-lvl2/__fixtures__/before.json',
+    extension);
+  const pathAfter = saveConfigFile('/Users/imac/Hexlet/frontend-project-lvl2/__fixtures__/after.json',
+    extension);
+
+  const answer = fs
+    .readFileSync('/Users/imac/Hexlet/frontend-project-lvl2/__fixtures__/answer.json', 'utf8');
+
+  expect(getDiff(pathBefore, pathAfter, 'json')).toEqual(answer);
 });
