39ce60f745ea4f296fcf704c10b6a9bc19caea77
marazmatique <fxpnkt@gmail.com>
2020-03-02 17:46:12 +0300

    porting cli-util to the launcher

--- src/bin/gendiff.js
+++ src/bin/gendiff.js
@@ -1,5 +1,14 @@
 #!/usr/bin/env node
 
-import gendiff from '..';
+import commander from 'commander';
+import getDiff from '..';
+
+const gendiff = () => new commander.Command()
+  .version('0.0.1')
+  .description('Compares two configuration files and shows a difference.')
+  .option('-f, --format [format]', 'write format options', 'total')
+  .arguments('<before> <after>')
+  .action((before, after, options) => console.log(getDiff(before, after, options.format)))
+  .parse(process.argv);
 
 gendiff();
