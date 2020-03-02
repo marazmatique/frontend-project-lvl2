b3efb3add329a92a795613d8f7504102dcc82790
marazmatique <fxpnkt@gmail.com>
2020-03-02 17:48:00 +0300

    porting general function to main module

--- src/index.js
+++ src/index.js
@@ -1,14 +1,49 @@
-import commander from 'commander';
-import getDiff from './getDiff';
+import _ from 'lodash';
+import getData from './parsers';
+import totalDiff from './formatters/total';
+import plainDiff from './formatters/plain';
+import jsonDiff from './formatters/json';
 
-export default () => {
-  const gendiff = new commander.Command();
+export const isObject = (ele) => typeof ele === 'object' && ele !== null;
 
-  gendiff
-    .version('0.0.1')
-    .description('fdfdf')
-    .option('-f, --format [format]', 'write format options', 'total')
-    .arguments('<before> <after>')
-    .action((before, after, options) => console.log(getDiff(before, after, options.format)))
-    .parse(process.argv);
+const buildAst = (obj1, obj2) => {
+  const keys = Object.keys({ ...obj1, ...obj2 });
+  return keys.reduce((acc, key) => {
+    const before = obj1[key];
+    const after = obj2[key];
+
+    if (isObject(before) && isObject(after)) {
+      acc.push([key, ' ', buildAst(before, after)]);
+      return acc;
+    }
+    if (before === after) {
+      acc.push([key, ' ', before]);
+      return acc;
+    }
+    if (_.has(obj1, key) && _.has(obj2, key)) {
+      acc.push([key, 'changed', before, after]);
+      return acc;
+    }
+    if (_.has(obj1, key)) {
+      acc.push([key, '-', before]);
+    }
+    if (_.has(obj2, key)) {
+      acc.push([key, '+', after]);
+    }
+
+    return acc;
+  }, []);
+};
+
+const outputFormat = {
+  total: (ast) => totalDiff(ast),
+  plain: (ast) => plainDiff(ast),
+  json: (ast) => jsonDiff(ast),
+};
+
+export default (pathToConfigFile1, pathToConfigFile2, format) => {
+  const obj1 = getData(pathToConfigFile1);
+  const obj2 = getData(pathToConfigFile2);
+  const ast = buildAst(obj1, obj2);
+  return outputFormat[format](ast);
 };
