022d67998b94e0977ca722d2030abe4d10d0bb5d
marazmatique <fxpnkt@gmail.com>
2020-03-08 18:33:13 +0300

    path building refactoring

--- __tests__/getDiff.test.js
+++ __tests__/getDiff.test.js
@@ -1,16 +1,20 @@
 import fs from 'fs';
+import path from 'path';
 import getDiff from '../src';
+
+const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
+const readFile = (filename) => fs.readFileSync(getFixturePath(filename), 'utf-8');
 
 test.each([
   ['plain', ['.json', '.yml', '.ini'], 'answer_plain'],
   ['total', ['.json', '.yml', '.ini'], 'answer_total'],
   ['json', ['.json', '.yml', '.ini'], 'answer.json'],
 ])('%s(%s)', (format, extensions, ans) => {
+  const answer = readFile(ans);
+
   extensions.forEach((ext) => {
-    const before = `${__dirname}/../__fixtures__/before${ext}`;
-    const after = `${__dirname}/../__fixtures__/after${ext}`;
-    const answer = fs
-      .readFileSync(`${__dirname}/../__fixtures__/${ans}`, 'utf8');
+    const before = getFixturePath(`before${ext}`);
+    const after = getFixturePath(`after${ext}`);
 
     expect(getDiff(before, after, format)).toEqual(answer);
   });
