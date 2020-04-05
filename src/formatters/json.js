0771179cb6964fc3887b1849a0686f39290581fd
marazmatique <fxpnkt@gmail.com>
2020-04-05 20:19:46 +0300

    refactoring

--- src/formatters/json.js
+++ src/formatters/json.js
@@ -1,5 +1,3 @@
 const normalize = (value) => (!/[^0-9]/.test(value) ? parseInt(value, 10) : value);
 
-const iter = (ast) => JSON.stringify(ast, (key, value) => normalize(value), 2);
-
-export default iter;
+export default (ast) => JSON.stringify(ast, (key, value) => normalize(value), 2);
